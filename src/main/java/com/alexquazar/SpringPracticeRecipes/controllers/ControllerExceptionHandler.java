02f210a6a471ad0867835611173b6ea44f7e7d08
bryanmonterrosa <bryan_monterrosa@hotmail.com>
2023-09-11 19:46:03 +0000

    New bean with a route to api/recipes

--- src/main/java/com/alexquazar/SpringPracticeRecipes/controllers/ControllerExceptionHandler.java
+++ src/main/java/com/alexquazar/SpringPracticeRecipes/controllers/ControllerExceptionHandler.java
@@ -1,6 +1,10 @@
 package com.alexquazar.SpringPracticeRecipes.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -8,20 +12,17 @@ import lombok.extern.slf4j.Slf4j;
 @ControllerAdvice
 public class ControllerExceptionHandler {
     
-    // @ResponseStatus(HttpStatus.BAD_REQUEST)
-    // @ExceptionHandler(NumberFormatException.class)
-    // public ModelAndView handleNumberFormat(Exception exception) {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NumberFormatException.class)
+    public String handleNumberFormat(Exception exception, Model model) {
 
-    //     log.error("Handling Number Format Excepton");
-    //     String errorMessage = exception.getMessage();
-    //     log.error(errorMessage);
+        log.error("Handling Binding Format Excepton");
+        String errorMessage = exception.getMessage();
+        log.error(errorMessage);
 
-    //     ModelAndView modelAndView = new ModelAndView();
+        model.addAttribute("exception", exception);
 
-    //     modelAndView.setViewName("400error");
-    //     modelAndView.addObject("exception", exception);
+        return "400error";
 
-    //     return modelAndView;
-
-    // }
+    }
 }
